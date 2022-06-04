603be10d5642336b796d2d90579e49ac874d6710
skira <13675790+lschiavini@users.noreply.github.com>
2022-06-04 10:28:16 -0300

    ✨feature: Installing roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import { AppProps } from "next/app"
+import { AppProps } from "../../node_modules/next/app"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-}
+} // Reruns every change of screen
 
 export default MyApp
