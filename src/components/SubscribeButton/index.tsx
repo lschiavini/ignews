a80d430e715d87cf23a72d2a62afde5b205dac4d
skira <13675790+lschiavini@users.noreply.github.com>
2022-06-04 15:48:08 -0300

    ✨feature: Subscribe button

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,15 @@
+
+import styles from "./styles.module.scss"
+
+function SubscribeButton() {
+    return (
+        <button type="button"
+        className={styles.subscribeButton}
+        >
+
+            Subscribe Now
+        </button>
+    );
+}
+
+export default SubscribeButton;
