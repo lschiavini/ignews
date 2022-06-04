1aa2bea6477ed07903f43606d1d44a2d16dd2589
skira <13675790+lschiavini@users.noreply.github.com>
2022-06-04 10:30:56 -0300

    ✨feature: Dynamic titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import Head from "../../node_modules/next/head"
 import styles from "../styles/home.module.scss"
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Init | ig.news</title>
+      </Head>
     <h1
       className={styles.title}
     >
       Hello <span>World</span>
     </h1>
+    </>
   )
 }
